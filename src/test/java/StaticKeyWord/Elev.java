256b9908dbff13e8843b36c429cc2e4159b67eb3
DumitraAdrian <adrian.danyel92@gmail.com>
2023-12-14 17:23:58 +0200

    Am invatat despre Staticg

---
+++ src/test/java/StaticKeyWord/Elev.java
@@ -0,0 +1,27 @@
+package StaticKeyWord;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class Elev {
+
+    // Static = este un heyword care are ca scop sa pastreze valoarea unei informatii pentru toate instantele unei clase
+
+    // Static se poate aplica la variabile si metode
+
+    private  String numeElev;
+    private String prenumeElev;
+    private static String scoala="Liviu Rebreanu";
+    private static Integer nrElevi=0;
+
+    public Elev(String numeElev, String prenumeElev) {
+        this.numeElev = numeElev;
+        this.prenumeElev = prenumeElev;
+        nrElevi++;
+    }
+    public  void prezentareElev(){
+        System.out.println("Numele elevului este :" +numeElev);
+        System.out.println("Prenumele elevului este :" +prenumeElev);
+        System.out.println("Scoala elevului este :"+scoala);
+        System.out.println("Nr elevi inscrisi la scoala este:" +nrElevi);
+    }
+}
